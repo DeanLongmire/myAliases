ba38385e8287ec946ad747eb2e39605750fd027e
Dean Longmire <107720531+DeanLongmire@users.noreply.github.com>
2023-08-29 17:40:03 -0400

    Add co and auto-complete (#1)

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,7 @@
+co() {
+    git checkout "$1"
+}
+
 gogit() {
   'cd /git'
 }
@@ -15,3 +19,10 @@ gnew() {
   git checkout -b "$1"
   git push --set-upstream origin "$1"
 }
+
+_co_completion() {
+    local branches
+    branches=$(git branch -a | awk '{print $NF}' | sed 's|remotes/origin/||' | sort -u)
+    COMPREPLY=($(compgen -W "$branches" -- "${COMP_WORDS[1]}"))
+}
+complete -F _co_completion co
