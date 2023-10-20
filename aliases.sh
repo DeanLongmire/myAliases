e1715417dc3c2c568f362e14dab6cab6a4739438
Dean Longmire <deanlongmire100@gmail.com>
2023-10-20 15:23:25 -0400

    rename rb to cr for cargo run

--- aliases.sh
+++ aliases.sh
@@ -32,7 +32,7 @@ gup() {
   git pull --rebase && git remote update origin --prune && git fetch -p -t && for branch in $(git for-each-ref --format "%(refname) %(upstream:track)" refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
 }
 
-rb() {
+cr() {
   current_dir="$(basename "$PWD")"
 
   if [ -d "$current_dir" ]; then
